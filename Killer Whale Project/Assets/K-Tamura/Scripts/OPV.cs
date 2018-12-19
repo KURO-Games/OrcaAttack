d652c90adae94c3e1f2ec1bdd3999552b2f8061e
k-tamura98 <17c1020017tk@edu.tech.ac.jp>
2018-12-19 17:21:57 +0900

    Mov,images(rock,sea,sand) 追加 hpbar修正 Serialize 追加 OrcaPositon取り変更

---
+++ Killer Whale Project/Assets/K-Tamura/Scripts/OPV.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class OPV : MonoBehaviour {
+    [SerializeField]
+    private VideoClip vi;
+    [SerializeField]
+    private VideoSource vs;
+    [SerializeField]
+    private VideoPlayer Vplayer;
+    [SerializeField]
+    private string SceneName;
+    [SerializeField]
+    private AudioSource audioSource;
+    // Use this for initialization
+    private void Awake()
+    {
+        Vplayer.source = vs;
+        Vplayer.clip = vi;
+        Vplayer.Play();
+        Vplayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        Vplayer.EnableAudioTrack(0, true);
+        Vplayer.SetTargetAudioSource(0, audioSource);
+
+    }
+
+    void Start () {
+       
+        	
+	}
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+        if (Vplayer.time == vi.length)
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+}
--- Killer Whale Project/Assets/K-Tamura/Scripts/OrcaManager2.cs
+++ Killer Whale Project/Assets/K-Tamura/Scripts/OrcaManager2.cs
@@ -1,99 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.Apple;using UnityEngine.UI;
 
 public class OrcaManager2 : MonoBehaviour {
-    Vector3 ray;
-    Vector3 worlddir;
-    Vector3 firsttouch, secondrelease,thr;
-    [SerializeField]
-    private GameObject Orca;
-    [SerializeField]
-    private float OrcaSpeed=12,orcahippari=100;
-    [SerializeField]
-    public static bool orcayoko, orcatate;
-    
-    // Use this for initialization
-    void Start () {
-        worlddir = Vector3.zero;
-        orcayoko = true;orcatate = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-        Ra();
-       
-        Mousehantei();
-        Debug.Log(firsttouch+" "+secondrelease);
-        if(orcayoko==true&&orcatate==false){
-            worlddir.y = -2.9f;
-            OrcaYokoIdo();
-            
-        }
-        if(orcatate==true&&orcayoko==false){
-
-            if (worlddir.y>=-14.0f) {
-                OrcaTateIdo();
-            }
-        }
-        //Debug.DrawRay(ray.origin, ray.direction * 30.0f, Color.red, 0.0f);
-
+    Vector3 worlddir,mouseposition,firsttouch, secondrelease,thr, point = new Vector3();
+    [SerializeField]private GameObject Orca;
+    [SerializeField]private float OrcaSpeed=1f,orcahippari=100f;
+    [SerializeField]public static bool orcayoko, orcatate;
+    private Camera cam; bool botton=false;
+    public static string Nam;
+    void Start () {cam = Camera.main;mouseposition = Vector3.zero;worlddir = Vector3.zero;orcayoko = true;orcatate = false;}
+    void Update () { Touchpanel();Mousehantei();
+        //Debug.Log("Y"+orcayoko+" T"+orcatate+" F"+firsttouch+" S"+secondrelease+" W"+worlddir+" M"+mouseposition+" O"+Orca.transform.localPosition);
+        if (orcayoko == true && orcatate == false)OrcaYokoIdo();
+        if (orcatate == true && orcayoko == false)OrcaTateIdo();
     }
     /// <summary>
-    /// Ray取得(Update)
+    /// Touchpanel this instance.
     /// </summary>
-    void Ra(){
-        ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
-    
-
-   // worlddir = ray.direction;
-        worlddir *= OrcaSpeed;
-        worlddir.z = 0;
+    private void Touchpanel()
+    {mouseposition = Input.mousePosition;
+    if (Input.GetMouseButtonDown(0))
+        { worlddir = Camera.main.ScreenToWorldPoint(new Vector3(mouseposition.x, mouseposition.y, 16.1f));
+            if (worlddir.y <= -13.1) botton = true;
+            else botton = false;
+        }
+        if (Input.GetMouseButton(0))worlddir = Camera.main.ScreenToWorldPoint(new Vector3(mouseposition.x, mouseposition.y, 16.1f));
+        if(Input.GetMouseButtonUp(0))botton = false;
     }
     /// <summary>
     /// Orca横移動
     /// </summary>
-    void OrcaYokoIdo(){
-        if(Input.GetMouseButton(0))
-        Orca.transform.position = worlddir;
-    }
+    void OrcaYokoIdo(){if (Input.GetMouseButton(0)){if (botton != true){worlddir.y = -9f;Orca.transform.localPosition = worlddir;}}}
     /// <summary>
     /// Orca縦移動
     /// </summary>
-    void OrcaTateIdo(){
-        if (Input.GetMouseButtonUp(0))
-        {
-            
-            
-                GetComponent<Rigidbody>().AddForce(new Vector3(thr.x * orcahippari, thr.y * orcahippari, 0));
-            
-        }
-    }
+    void OrcaTateIdo(){if (Input.GetMouseButtonUp(0)){GetComponent<Rigidbody>().AddForce(new Vector3(thr.x * orcahippari, thr.y * orcahippari, 0));}}
     /// <summary>
     /// Mouse取得(Update)
     /// </summary>
     void Mousehantei(){
-        if(Input.GetMouseButtonDown(0)){
-            
-            
-            
-                firsttouch = worlddir;
-            
-
-        }
-        if(Input.GetMouseButton(0)){
-            
-            
-            secondrelease = worlddir;
-        }
-        if(Input.GetMouseButtonUp(0)){
-
-            
-            
-                thr = firsttouch - secondrelease;
-            
-        }
+        if(Input.GetMouseButtonDown(0)){//各判定mouseのposition(first)
+            firsttouch.x = mouseposition.x;firsttouch.y = mouseposition.y;firsttouch.z = 0;}
+        if(Input.GetMouseButton(0)){//各判定mouseのposition(second)
+            secondrelease.x = mouseposition.x;secondrelease.y = mouseposition.y;secondrelease.z = 0;}
+        if(Input.GetMouseButtonUp(0)){//各判定mouseのposition_vector3(final)
+            thr = firsttouch - secondrelease;}
     }
 }
