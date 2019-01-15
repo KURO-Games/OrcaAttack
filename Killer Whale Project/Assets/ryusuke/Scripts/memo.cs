a945c3f69e9b29b8cb276c265f0cdc285928606b
ryusuke <ryusuke0428tora@gmail.com>
2019-01-15 12:02:49 +0900

    メモの追加

---
+++ Killer Whale Project/Assets/ryusuke/Scripts/memo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class memo : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("Coroutine");
+	}
+
+    bool atari;
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            atari = true;
+        }
+
+        if (atari == true)
+        {
+
+            GetComponent<BoxCollider>().enabled = false;
+        }
+	}
+
+    IEnumerator Coroutine()
+    {
+
+        yield return new WaitForSeconds(1.0f);
+
+    }
+}
